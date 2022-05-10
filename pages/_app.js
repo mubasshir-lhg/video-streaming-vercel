3db4f21ed26d874c9a9bef0668a6d28149fd29e8
Victor <37217077+Muradalikhan@users.noreply.github.com>
2022-05-10 19:56:43 +0500

    created lay out

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import ThemeConfig from '../theme';
-// import ScrollToTop from '../components/scrollToTop';
+import Layout from '../layout/Layout';
 function MyApp({ Component, pageProps }) {
-  return <ThemeConfig>
-    {/* <ScrollToTop /> */}
-    <Component {...pageProps} />
-  </ThemeConfig>
+  return (
+    <ThemeConfig>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;

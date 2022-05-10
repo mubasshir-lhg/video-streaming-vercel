e2c96f7b6d3d5f90cae1b710f24f755517a110a3
Victor <37217077+Muradalikhan@users.noreply.github.com>
2022-05-10 19:53:47 +0500

    input style override

--- theme/overrides/Input.js
+++ theme/overrides/Input.js
@@ -2,64 +2,101 @@
 
 export default function Input(theme) {
   return {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          color: "#fff",
+        },
+      },
+    },
     MuiInputBase: {
       styleOverrides: {
         root: {
-          '&.Mui-disabled': {
-            '& svg': { color: theme.palette.text.disabled }
-          }
+          "&.Mui-disabled": {
+            "& svg": { color: theme.palette.text.disabled },
+          },
         },
         input: {
-          '&::placeholder': {
+          "&::placeholder": {
             opacity: 1,
-            color: theme.palette.text.disabled
-          }
-        }
-      }
+            color: theme.palette.text.disabled,
+          },
+        },
+      },
     },
     MuiInput: {
       styleOverrides: {
         underline: {
-          '&:before': {
-            borderBottomColor: theme.palette.grey[500_56]
-          }
-        }
-      }
+          "&:before": {
+            borderBottomColor: theme.palette.grey[500_56],
+          },
+        },
+      },
+    },
+    MuiSelect: {
+      styleOverrides: {
+        select: {
+          backgroundColor: theme.palette.secondary.main,
+          padding: "5px",
+          borderRadius: theme.shape.borderRadius,
+        },
+        icon: {
+          color: "#fff",
+        },
+      },
+    },
+    MuiMenu: {
+      styleOverrides: {
+        list: {
+          backgroundColor: theme.palette.secondary.main,
+        },
+      },
+    },
+    MuiPopover: {
+      styleOverrides: {
+        paper: {
+          boxShadow: theme.customShadows.z8,
+          backgroundColor: theme.palette.secondary.main,
+        },
+      },
     },
     MuiFilledInput: {
       styleOverrides: {
         root: {
-          backgroundColor: theme.palette.grey[500_12],
-          '&:hover': {
-            backgroundColor: theme.palette.grey[500_16]
+          backgroundColor: theme.palette.grey[100],
+          "&:hover": {
+            backgroundColor: theme.palette.grey[500_16],
           },
-          '&.Mui-focused': {
-            backgroundColor: theme.palette.action.focus
+          "&.Mui-focused": {
+            backgroundColor: theme.palette.action.focus,
           },
-          '&.Mui-disabled': {
-            backgroundColor: theme.palette.action.disabledBackground
-          }
+          "&.Mui-disabled": {
+            backgroundColor: theme.palette.action.disabledBackground,
+          },
         },
         underline: {
-          '&:before': {
-            borderBottomColor: theme.palette.grey[500_56]
-          }
-        }
-      }
+          "&.MuiFilledInput-underline": {
+            disableUnderline: true,
+          },
+          "&:before": {
+            borderBottomColor: theme.palette.grey[500_56],
+          },
+        },
+      },
     },
     MuiOutlinedInput: {
       styleOverrides: {
         root: {
-          '& .MuiOutlinedInput-notchedOutline': {
-            borderColor: theme.palette.grey[500_32]
+          "& .MuiOutlinedInput-notchedOutline": {
+            borderColor: theme.palette.grey[500_32],
           },
-          '&.Mui-disabled': {
-            '& .MuiOutlinedInput-notchedOutline': {
-              borderColor: theme.palette.action.disabledBackground
-            }
-          }
-        }
-      }
-    }
+          "&.Mui-disabled": {
+            "& .MuiOutlinedInput-notchedOutline": {
+              borderColor: theme.palette.action.disabledBackground,
+            },
+          },
+        },
+      },
+    },
   };
 }

9faaf8dc64f50ced6ee99c3994e9d434ceeb4c48
muhammad affan <affanmuhammad623@gmail.com>
2022-05-12 06:06:55 -0700

    custom button and theme changes

--- components/Button/OutlinedButton.js
+++ components/Button/OutlinedButton.js
@@ -1,13 +1,22 @@
 import Button from '@mui/material/Button';
-const OutlinedButton = ({text,width,color}) => {
+import React, { useState } from 'react';
+import { useTheme } from "@mui/styles";
+
+const OutlinedButton = ({ text, width, color, active, onClick }) => {
+  const { palette } = useTheme();
+
+  let activeColor = ''
+  color ==='primary' ? activeColor = palette.gradients.purple : color ==='yellow' ? activeColor = palette.gradients.yellow : color ==='info' ? activeColor = palette.gradients.info :color ==='warning' ? activeColor = palette.gradients.warning :color ==='success' ? activeColor = palette.gradients.success : activeColor = ''
+ 
   return (
-    <Button variant='outlined' color={color}
+    <Button variant='outlined' color={color} onClick={onClick}
       sx={{
         px: 2,
         color: 'white',
         minWidth: "120px",
         minHeight: "40px",
-        width:`${width}`,
+        width: `${width}`,
+        background: active && activeColor,
       }}  >
             {text}
         </Button>
--- theme/shape.js
+++ theme/shape.js
@@ -1,6 +1,7 @@
 // ----------------------------------------------------------------------
 
 const shape = {
+  borderRadiusXs: 2,
   borderRadius: 4,
   borderRadiusSm: 8,
   borderRadiusMd: 12

c1bedcdc0c3cdd4a7b658466d6305f619b95f482
Murad <murad.jaddoon@gmail.com>
2022-05-13 12:49:06 +0500

    line chart data

---
+++ components/Chart/data.js
@@ -0,0 +1,39 @@
+
+
+export const userData=[
+    {
+        id:1,
+        date:'May 27,2022',
+        views:80000,
+    },
+    {
+        id:2,
+        date:'Nov 27,2022',
+        views:100000,
+    },
+    {
+        id:3,
+        date:'Oct 27,2022',
+        views:80000,
+    },
+    {
+        id:4,
+        date:'Dec 27,2022',
+        views:120000,
+    },
+    {
+        id:5,
+        date:'Apr 27,2022',
+        views:50000,
+    },
+    {
+        id:5,
+        date:'Sep 27,2022',
+        views:50000,
+    },
+    {
+        id:5,
+        date:'Mar 27,2022',
+        profit:50000,
+    },
+]

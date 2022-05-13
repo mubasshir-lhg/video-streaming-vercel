0034b42204e3a4aa5bd574001050a1c60e10d608
Murad <murad.jaddoon@gmail.com>
2022-05-13 19:05:15 +0500

    formate updated

--- components/Category/Category.js
+++ components/Category/Category.js
@@ -1,37 +1,55 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 import { Box, Grid } from "@mui/material";
-import IndividualCategory from './IndividualCategory';
+import IndividualCategory from "./IndividualCategory";
 import { styled } from "@mui/system";
 
 const Category = () => {
-    const [activeBtn, setActivebtn] = useState(0)
-    
-    const handleActiveBtn = (index) => {
-        setActivebtn(index);
-    }
+  const [activeBtn, setActivebtn] = useState(0);
 
+  const handleActiveBtn = (index) => {
+    setActivebtn(index);
+  };
 
-    const LinksWrapper = styled(Box)({
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "flex-start",
-        gap: "50px",
-        marginBottom: "20px",
-        overflowX: 'scroll',
-        maxWidth: "100vw",
-        '&::-webkit-scrollbar': {
-            display: 'none'
-          }
-      });
+  const LinksWrapper = styled(Box)({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: "50px",
+    marginBottom: "20px",
+    overflowX: "scroll",
+    maxWidth: "100vw",
+    "&::-webkit-scrollbar": {
+      display: "none",
+    },
+  });
 
-    const obj = [{ text: 'all', color: 'primary' }, { text: 'yellow', color: 'yellow' }, { text: 'info', color: 'info' }, { text: 'success', color: 'success' }, { text: 'warning', color: 'warning' }, { text: 'all', color: 'primary' }, { text: 'yellow', color: 'yellow' }, { text: 'info', color: 'info' }, { text: 'success', color: 'success' }, { text: 'warning', color: 'warning' }];
-    return (<LinksWrapper  
-        >
-        {obj.map((data,index)=>{return <IndividualCategory onClick={()=>handleActiveBtn(index)} active={activeBtn==index} text={data.text} color={data.color} key={index} />})}
+  const obj = [
+    { text: "all", color: "primary" },
+    { text: "yellow", color: "yellow" },
+    { text: "info", color: "info" },
+    { text: "success", color: "success" },
+    { text: "warning", color: "warning" },
+    { text: "all", color: "primary" },
+    { text: "yellow", color: "yellow" },
+    { text: "info", color: "info" },
+    { text: "success", color: "success" },
+    { text: "warning", color: "warning" },
+  ];
+  return (
+    <LinksWrapper>
+      {obj.map((data, index) => {
+        return (
+          <IndividualCategory
+            onClick={() => handleActiveBtn(index)}
+            active={activeBtn == index}
+            text={data.text}
+            color={data.color}
+            key={index}
+          />
+        );
+      })}
+    </LinksWrapper>
+  );
+};
 
-  </LinksWrapper >
-  )
-}
-
-export default Category
- 
+export default Category;

ce17d39231ef048ec50e8951a9fa014f38c3da97
Murad <murad.jaddoon@gmail.com>
2022-05-13 19:04:53 +0500

    formate updated

--- components/Button/OutlinedButton.js
+++ components/Button/OutlinedButton.js
@@ -1,26 +1,40 @@
-import Button from '@mui/material/Button';
-import React, { useState } from 'react';
+import Button from "@mui/material/Button";
+import React, { useState } from "react";
 import { useTheme } from "@mui/styles";
 
 const OutlinedButton = ({ text, width, color, active, onClick }) => {
   const { palette } = useTheme();
 
-  let activeColor = ''
-  color ==='primary' ? activeColor = palette.gradients.purple : color ==='yellow' ? activeColor = palette.gradients.yellow : color ==='info' ? activeColor = palette.gradients.info :color ==='warning' ? activeColor = palette.gradients.warning :color ==='success' ? activeColor = palette.gradients.success : activeColor = ''
- 
+  let activeColor = "";
+  color === "primary"
+    ? (activeColor = palette.gradients.purple)
+    : color === "yellow"
+    ? (activeColor = palette.gradients.yellow)
+    : color === "info"
+    ? (activeColor = palette.gradients.info)
+    : color === "warning"
+    ? (activeColor = palette.gradients.warning)
+    : color === "success"
+    ? (activeColor = palette.gradients.success)
+    : (activeColor = "");
+
   return (
-    <Button variant='outlined' color={color} onClick={onClick}
+    <Button
+      variant="outlined"
+      color={color}
+      onClick={onClick}
       sx={{
         px: 2,
-        color: 'white',
+        color: "white",
         minWidth: "120px",
         minHeight: "40px",
         width: `${width}`,
         background: active && activeColor,
-      }}  >
-            {text}
-        </Button>
-  )
-}
+      }}
+    >
+      {text}
+    </Button>
+  );
+};
 
-export default OutlinedButton
+export default OutlinedButton;

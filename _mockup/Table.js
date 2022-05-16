e0f48d9bc01e5aa268b5aa4fee5b142066b6548d
Murad <murad.jaddoon@gmail.com>
2022-05-16 21:27:23 +0500

    table data created

---
+++ _mockup/Table.js
@@ -0,0 +1,29 @@
+import img1 from "../assets/Images/user1.jpg";
+import img2 from "../assets/Images/user2.jpg";
+
+export const coloms = ["Username", "Date Subscribed", "Likes"];
+export const coloms2 = ["Videos", "Title", "Views", "Upload Date"];
+
+export const rows = [
+  { img: img1, name: "Sippoy Casella", date: "Jan 24,2021", likes: 513 },
+  { img: img2, name: "Sippoy Casella", date: "Jan 24,2021", likes: 513 },
+  { img: img1, name: "Sippoy Casella", date: "Jan 24,2021", likes: 513 },
+  { img: img2, name: "Sippoy Casella", date: "Jan 24,2021", likes: 513 },
+  { img: img1, name: "Sippoy Casella", date: "Jan 24,2021", likes: 513 },
+  { img: img2, name: "Jonadrielable", date: "Jan 24,2021", likes: 513 },
+  { img: img1, name: "Jonadrielable", date: "Jan 24,2021", likes: 513 },
+  { img: img2, name: "Jonadrielable", date: "Jan 24,2021", likes: 513 },
+  { img: img2, name: "Jonadrielable", date: "Jan 24,2021", likes: 513 },
+  { img: img2, name: "Jonadrielable", date: "Jan 24,2021", likes: 513 },
+];
+export const rows2 = [
+  { img: img1, title: "MacBook Pro", views: 513, date: "Jan 24,2021" },
+  { img: img1, title: "MacBook Pro", views: 513, date: "Jan 24,2021" },
+  { img: img1, title: "MacBook Pro", views: 513, date: "Jan 24,2021" },
+  { img: img1, title: "MacBook Pro Max", views: 613, date: "May 24,2022" },
+  { img: img1, title: "MacBook Pro Max", views: 613, date: "May 24,2022" },
+  { img: img1, title: "MacBook Pro Max", views: 613, date: "May 24,2022" },
+  { img: img1, title: "MacBook", views:5000, date: "June 24,2022" },
+  { img: img1, title: "MacBook", views:5000, date: "June 24,2022" },
+  { img: img1, title: "MacBook", views:5000, date: "June 24,2022" },
+];

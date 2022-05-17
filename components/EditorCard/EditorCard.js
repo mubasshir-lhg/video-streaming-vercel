23ae21deb4b759a2d7ed1a57770b37186e5cd971
Murad <murad.jaddoon@gmail.com>
2022-05-17 18:46:58 +0500

    editor card component created

---
+++ components/EditorCard/EditorCard.js
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  Box,
+  Grid,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  Typography,
+} from "@mui/material";
+import Image from "next/image";
+//icon
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
+const EditorCard = ({ value }) => {
+  return (
+    <Grid
+      container
+      alignItems="center"
+      justifyContent="center"
+      sx={{ py: 2, borderTop: "1px solid #fff" }}
+      spacing={1}
+    >
+      <Grid item xs={1}>
+        <FormGroup>
+          <FormControlLabel control={<Checkbox color="purple" />} />
+        </FormGroup>
+      </Grid>
+      <Grid item sm={4} md={4} lg={2}>
+        <Box sx={{ minWidth: "100px", mr: 2 }}>
+          <Image
+            alt="table-img"
+            src={value.img}
+            width={150}
+            height={100}
+            quality={100}
+            objectFit="cover"
+            className="bar-img"
+          />
+        </Box>
+      </Grid>
+      <Grid item xs={10} sm={8} lg={7} order={{ xs: 3, md: 3, lg: 1 }}>
+        {value.videoTitle}
+        <Typography variant="body2">
+          {value.videoViews} . {value.videoUploadTime}
+        </Typography>
+      </Grid>
+      <Grid item xs={2} sm={2} md={2} lg={1} order={{ xs: 2, md: 2, lg: 2 }}>
+        {value.views}
+      </Grid>
+      <Grid item xs={3} md={3} lg={1} order={{ xs: 1, md: 1, lg: 2 }}>
+        <Grid container>
+          <Grid item xs={6}>
+            <ChatBubbleIcon />
+          </Grid>
+          <Grid item xs={6}>
+            {value.comments}
+          </Grid>
+          <Grid item xs={6}>
+            <ThumbUpAltIcon />
+          </Grid>
+          <Grid item xs={6}>
+            {value.likes}
+          </Grid>
+          <Grid item xs={6}>
+            <ThumbDownAltIcon />
+          </Grid>
+          <Grid item xs={6}>
+            {value.dislikes}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default EditorCard;

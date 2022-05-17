2cd658fe444de7efd1a3ae7adf7812009cb72d49
Murad <murad.jaddoon@gmail.com>
2022-05-17 18:46:33 +0500

    editor data created

---
+++ _mockup/Editor.js
@@ -0,0 +1,52 @@
+import img from "../assets/Images/user1.jpg";
+
+export const editorData = [
+  {
+    id: 1,
+    img: img,
+    videoTitle:
+      "Top 10 Travel for beginners Travel Thailand, Bail, Phuket,Maldives, Andamans and more",
+    videoViews: "267k views",
+    videoUploadTime: "3 months",
+    views: "7 views",
+    comments: 20,
+    likes: 135,
+    dislikes: 2,
+  },
+  {
+    id: 1,
+    img: img,
+    videoTitle:
+      "Top 10 Travel for beginners Travel Thailand, Bail, Phuket,Maldives, Andamans and more",
+    videoViews: "267k views",
+    videoUploadTime: "3 months",
+    views: "7 views",
+    comments: 20,
+    likes: 135,
+    dislikes: 2,
+  },
+  {
+    id: 1,
+    img: img,
+    videoTitle:
+      "Top 10 Travel for beginners Travel Thailand, Bail, Phuket,Maldives, Andamans and more",
+    videoViews: "267k views",
+    videoUploadTime: "3 months",
+    views: "7 views",
+    comments: 20,
+    likes: 135,
+    dislikes: 2,
+  },
+  {
+    id: 1,
+    img: img,
+    videoTitle:
+      "Top 10 Travel for beginners Travel Thailand, Bail, Phuket,Maldives, Andamans and more",
+    videoViews: "267k views",
+    videoUploadTime: "3 months",
+    views: "7 views",
+    comments: 20,
+    likes: 135,
+    dislikes: 2,
+  },
+];

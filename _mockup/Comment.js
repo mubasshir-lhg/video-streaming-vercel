96e59b3b6f909dd29ca219149ab0c9790d13daca
Murad <murad.jaddoon@gmail.com>
2022-05-18 17:11:45 +0500

    created mockuo for comment page

---
+++ _mockup/Comment.js
@@ -0,0 +1,49 @@
+import user1 from "../assets/Images/user1.jpg";
+import user2 from "../assets/Images/user2.jpg";
+
+export const CommentData = [
+  {
+    id: 1,
+    img: user1,
+    userName: "John Doe",
+    time: "2 minutes ago",
+    tag: "Top ten travel tips",
+    comment:
+      " Ullamco sunt quis ut qui Cupidatat anim nulla labore minim anim officia deserunt nostrud aliqua laborum nisi cillum.Nisi quis labore elit deserunt veniam esse dolore non dolor. eiusmod amet ad qui.Nis qui irure amet commodo aliqua.",
+    moreDetail:
+      " more details : Duis Lorem commodo et ipsum cupidatat officia.Mollit laborum velit",
+  },
+  {
+    id: 2,
+    img: user2,
+    userName: "John Doe",
+    time: "2 minutes ago",
+    tag: "Top ten travel tips",
+    comment:
+      " Ullamco sunt quis ut qui Cupidatat anim nulla labore minim anim officia deserunt nostrud aliqua laborum nisi cillum.Nisi quis labore elit deserunt veniam esse dolore non dolor. eiusmod amet ad qui.Nis qui irure amet commodo aliqua.",
+    moreDetail:
+      " more details : Duis Lorem commodo et ipsum cupidatat officia.Mollit laborum velit",
+  },
+  {
+    id: 3,
+    img: user1,
+    userName: "John Doe",
+    time: "2 minutes ago",
+    tag: "Top ten travel tips",
+    comment:
+      " Ullamco sunt quis ut qui Cupidatat anim nulla labore minim anim officia deserunt nostrud aliqua laborum nisi cillum.Nisi quis labore elit deserunt veniam esse dolore non dolor. eiusmod amet ad qui.Nis qui irure amet commodo aliqua.",
+    moreDetail:
+      " more details : Duis Lorem commodo et ipsum cupidatat officia.Mollit laborum velit",
+  },
+  {
+    id: 4,
+    img: user2,
+    userName: "John Doe",
+    time: "2 minutes ago",
+    tag: "Top ten travel tips",
+    comment:
+      " Ullamco sunt quis ut qui Cupidatat anim nulla labore minim anim officia deserunt nostrud aliqua laborum nisi cillum.Nisi quis labore elit deserunt veniam esse dolore non dolor. eiusmod amet ad qui.Nis qui irure amet commodo aliqua.",
+    moreDetail:
+      " more details : Duis Lorem commodo et ipsum cupidatat officia.Mollit laborum velit",
+  },
+];

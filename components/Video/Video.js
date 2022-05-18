acaf332e6a37be7c39d3195c51199368b27b04f2
Murad <murad.jaddoon@gmail.com>
2022-05-18 19:38:44 +0500

    video component updated

--- components/Video/Video.js
+++ components/Video/Video.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Box } from "@mui/material";
 import { useTheme } from "@mui/styles";
-const Video = ({ src }) => {
+const Video = ({ src, width, height,borderRadius }) => {
   const { shape, customShadows } = useTheme();
   return (
-    <Box sx={{ width: 240, height: 140, my: 2, }}>
+    <Box sx={{ width: width || 240, height: height || 140, my: 2 }}>
       <video
         width="100%"
         controls
         style={{
-          borderRadius: shape.borderRadiusMd,
+          borderRadius:borderRadius|| shape.borderRadiusMd,
           boxShadow: customShadows.z1,
         }}
       >

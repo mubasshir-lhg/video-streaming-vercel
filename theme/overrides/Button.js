b63c6d4b36c75df6f39779bf2cdecdb5d1fc8bf5
Murad <murad.jaddoon@gmail.com>
2022-05-19 20:35:55 +0500

    override button styles

--- theme/overrides/Button.js
+++ theme/overrides/Button.js
@@ -2,96 +2,135 @@
 
 export default function Button(theme) {
   return {
-    MuiIconButton:{
-      styleOverrides:{
-        root:{
-          color:'#fff'
-        }
-      }
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          color: "#fff",
+        },
+      },
     },
     MuiButton: {
       styleOverrides: {
         root: {
-          '&:hover': {
-            boxShadow: 'none'
-          }
+          color:theme.palette.common.white,
+          "&:hover": {
+            boxShadow: "none",
+          },
         },
         sizeLarge: {
-          height: 48
+          height: 48,
         },
         containedInherit: {
           backgroundColor: theme.palette.grey[100],
-          color: theme.palette.grey[800],
-          boxShadow:'none',
-          '&:hover': {
-            backgroundColor: theme.palette.grey[500_16]
-          }
+          boxShadow: "none",
+          "&:hover": {
+            backgroundColor: theme.palette.grey[500_16],
+          },
         },
         containedPrimary: {
-          boxShadow: theme.customShadows.purple,
+          background: theme.palette.gradients.primary,
+          border: `1px solid ${theme.palette.secondary.main} `,
+          "&:hover": {
+            background: "transparent",
+          },
         },
         containedSecondary: {
-          boxShadow: theme.customShadows.secondary
+          background: theme.palette.gradients.secondary,
+          border: `1px solid ${theme.palette.secondary.main} `,
+          "&:hover": {
+            background: "transparent",
+          },
         },
         containedInfo: {
-          backgroundColor: theme.palette.grey[50]
+          background: theme.palette.gradients.info,
+          border: `1px solid ${theme.palette.info.main} `,
+          "&:hover": {
+            background: "transparent",
+          },
         },
         containedPurple: {
           background: theme.palette.gradients.purple,
+          border: `1px solid ${theme.palette.purple.main} `,
           "&:hover": {
             background: "transparent",
-            border: `1px solid ${theme.palette.purple.main} ` 
-          }
+          },
         },
         containedWarning: {
           background: theme.palette.gradients.warning,
+          border: `1px solid ${theme.palette.warning.main} `,
           "&:hover": {
             background: "transparent",
-            border: `1px solid ${theme.palette.warning.main} ` 
-          }
+          },
+        },
+        containedYellow: {
+          background: theme.palette.gradients.yellow,
+          border: `1px solid ${theme.palette.yellow.main} `,
+          "&:hover": {
+            background: "transparent",
+          },
+        },
+        containedSuccess: {
+          background: theme.palette.gradients.success,
+          border: `1px solid ${theme.palette.success.main} `,
+          "&:hover": {
+            background: "transparent",
+          },
         },
         outlinedPrimary: {
-          border: `1px solid ${theme.palette.purple.main} ` ,
+          border: `1px solid ${theme.palette.purple.main} `,
           "&:hover": {
-            background: theme.palette.gradients.purple, 
-          }
+            background: theme.palette.gradients.purple,
+          },
+        },
+        outlinedSecondary: {
+          border: `1px solid ${theme.palette.common.white} `,
+          background: "transparent",
+          "&:hover": {
+            background: theme.palette.secondary.main,
+          },
         },
         outlinedYellow: {
-          border: `1px solid ${theme.palette.yellow.main} ` ,
+          border: `1px solid ${theme.palette.yellow.main} `,
           "&:hover": {
-            background: theme.palette.gradients.yellow, 
-          }
+            background: theme.palette.gradients.yellow,
+          },
         },
         outlinedWarning: {
-          border: `1px solid ${theme.palette.warning.main} ` ,
+          border: `1px solid ${theme.palette.warning.main} `,
           "&:hover": {
-            background: theme.palette.gradients.warning, 
-          }
+            background: theme.palette.gradients.warning,
+          },
         },
         outlinedSuccess: {
-          border: `1px solid ${theme.palette.success.main} ` ,
+          border: `1px solid ${theme.palette.success.main} `,
           "&:hover": {
-            background: theme.palette.gradients.success, 
-          }
+            background: theme.palette.gradients.success,
+          },
         },
         outlinedInfo: {
-          border: `1px solid ${theme.palette.info.main} ` ,
+          border: `1px solid ${theme.palette.info.main} `,
           "&:hover": {
-            background: theme.palette.gradients.info, 
-          }
+            background: theme.palette.gradients.info,
+          },
+        },
+        outlinedPurple: {
+          border: `1px solid ${theme.palette.purple.main} `,
+          "&:hover": {
+            background: theme.palette.gradients.purple,
+          },
         },
         outlinedInherit: {
           border: `1px solid ${theme.palette.grey[500_32]}`,
-          '&:hover': {
-            backgroundColor: theme.palette.action.hover
-          }
+          "&:hover": {
+            backgroundColor: theme.palette.action.hover,
+          },
         },
         textInherit: {
-          '&:hover': {
-            backgroundColor: theme.palette.action.hover
-          }
-        }
-      }
-    }
+          "&:hover": {
+            backgroundColor: theme.palette.action.hover,
+          },
+        },
+      },
+    },
   };
 }

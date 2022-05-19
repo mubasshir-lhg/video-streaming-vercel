bd50b7bc63e7489f486d24b94ec5830f2efe1782
Murad <murad.jaddoon@gmail.com>
2022-05-19 19:38:07 +0500

    set backdrop of modal as per theme

--- theme/overrides/Backdrop.js
+++ theme/overrides/Backdrop.js
@@ -1,17 +1,18 @@
 import { alpha } from '@mui/material/styles';
 
 // ----------------------------------------------------------------------
+// background: linear-gradient(75deg, rgb(0 27 66 / 70%) 0%, rgb(14 28 48) 100%);
 
 export default function Backdrop(theme) {
-  const varLow = alpha(theme.palette.grey[900], 0.48);
-  const varHigh = alpha(theme.palette.grey[900], 1);
+  const varLow = alpha(theme.palette.primary.darker, 0.80);
+  const varHigh = alpha(theme.palette.primary.darker, 1);
 
   return {
     MuiBackdrop: {
       styleOverrides: {
         root: {
           background: [
-            `rgb(22,28,36)`,
+            `rgb(0,27,66)`,
             `-moz-linear-gradient(75deg, ${varLow} 0%, ${varHigh} 100%)`,
             `-webkit-linear-gradient(75deg, ${varLow} 0%, ${varHigh} 100%)`,
             `linear-gradient(75deg, ${varLow} 0%, ${varHigh} 100%)`

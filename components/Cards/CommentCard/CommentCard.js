0c5d4d47399dbcce1793118e28c8193e7f0409ba
Murad <murad.jaddoon@gmail.com>
2022-05-20 19:08:31 +0500

    updated comment card to fix responsiveness

--- components/Cards/CommentCard/CommentCard.js
+++ components/Cards/CommentCard/CommentCard.js
@@ -6,11 +6,20 @@ import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import FlagIcon from "@mui/icons-material/Flag";
-import { Typography, Box, IconButton, Grid } from "@mui/material";
+import {
+  Typography,
+  Box,
+  IconButton,
+  Grid,
+  useMediaQuery,
+} from "@mui/material";
+import { useTheme } from "@mui/system";
 import Image from "next/image";
 
 const CommentCard = ({ item }) => {
   const [open, setOpen] = useState(false);
+  const { breakpoints } = useTheme();
+  const isMdDown = useMediaQuery(breakpoints.down("md"));
   const handleClick = () => {
     setOpen(!open);
   };
@@ -18,18 +27,18 @@ const CommentCard = ({ item }) => {
   return (
     <Box py={2} px={4} sx={{ "&:hover": { backgroundColor: "primary.light" } }}>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={1}>
+        <Grid item xs={4} md={2} lg={1}>
           <Image
             src={item.img}
-            width={80}
-            height={80}
+            width={isMdDown ? 120 : 80}
+            height={isMdDown ? 120 : 80}
             objectFit="cover"
             quality={100}
             alt="img"
             className="bar-img"
           />
         </Grid>
-        <Grid item xs={8}>
+        <Grid item xs={8} md={7} lg={8}>
           <Box sx={{ pb: 1, display: "flex", gap: { xs: "10px", md: "30px" } }}>
             <Typography>{item.userName}</Typography>
             <Typography variant="body2">{item.time}</Typography>
@@ -37,7 +46,7 @@ const CommentCard = ({ item }) => {
           </Box>
           {item.comment}
         </Grid>
-        <Grid item xs={1}>
+        <Grid item xs={1} sx={{ml:'auto'}}>
           <IconButton>
             <ThumbUpIcon />
           </IconButton>

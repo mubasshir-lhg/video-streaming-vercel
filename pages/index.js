efc024fa89ea88191e1214a7909e790929cf30e1
Murad <murad.jaddoon@gmail.com>
2022-06-01 16:51:33 +0500

    created home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
-import { Box   } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import HomeCard from "../components/Cards/HomeCard/HomeCard";
 import Category from "../components/Category/Category";
+import { homedata } from "../_mockup/Home";
 
 const homepage = () => {
-      
-    return (<Box>
-        <Category/>
+  return (
+    <Box>
+      {/* <Category /> */}
+      <Grid container spacing={2}>
+        {homedata?.map((item, index) => (
+          <Grid item xs={12} md={6} lg={3} key={index}>
+            <HomeCard item={item}/>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
-  )
-}
+  );
+};
 
-export default homepage
+export default homepage;

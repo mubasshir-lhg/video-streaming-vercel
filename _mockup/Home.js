8aa45302f1333dbba886a611f327325f95bc21b0
Murad <murad.jaddoon@gmail.com>
2022-06-01 16:51:10 +0500

    created home data mockup

---
+++ _mockup/Home.js
@@ -0,0 +1,82 @@
+export const homedata=[
+    {
+        id:1,
+        videoSrc:'video1.mp4',
+        authorImg:'user1.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'John Doe Vlogs',
+        views:'267k',
+        uploadTime:'3months',
+        duration:'4:00'
+    },
+    {
+        id:2,
+        videoSrc:'video3.mp4',
+        authorImg:'user2.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'Lucy Gray Vlogs',
+        views:'569k',
+        uploadTime:'4months',
+        duration:'4:00'
+    },
+    {
+        id:1,
+        videoSrc:'video1.mp4',
+        authorImg:'user1.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'John Doe Vlogs',
+        views:'267k',
+        uploadTime:'3months',
+        duration:'4:00'
+    },
+    {
+        id:2,
+        videoSrc:'video3.mp4',
+        authorImg:'user2.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'Lucy Gray Vlogs',
+        views:'569k',
+        uploadTime:'4months',
+        duration:'4:00'
+    },
+    {
+        id:1,
+        videoSrc:'video1.mp4',
+        authorImg:'user1.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'John Doe Vlogs',
+        views:'267k',
+        uploadTime:'3months',
+        duration:'4:00'
+    },
+    {
+        id:2,
+        videoSrc:'video3.mp4',
+        authorImg:'user2.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'Lucy Gray Vlogs',
+        views:'569k',
+        uploadTime:'4months',
+        duration:'4:00'
+    },
+    {
+        id:1,
+        videoSrc:'video1.mp4',
+        authorImg:'user1.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'John Doe Vlogs',
+        views:'267k',
+        uploadTime:'3months',
+        duration:'4:00'
+    },
+    {
+        id:2,
+        videoSrc:'video3.mp4',
+        authorImg:'user2.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'Lucy Gray Vlogs',
+        views:'569k',
+        uploadTime:'4months',
+        duration:'4:00'
+    },
+]

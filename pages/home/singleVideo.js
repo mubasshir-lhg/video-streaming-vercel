e50cff2c87550ee2e4f5cee2d9db957a3cafbb04
Murad <murad.jaddoon@gmail.com>
2022-06-02 20:19:50 +0500

    added add comment component to single video page

--- pages/home/singleVideo.js
+++ pages/home/singleVideo.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withRouter } from "next/router";
 import { Grid } from "@mui/material";
 import VideoToPlay from "../../components/Video/videoToPlay/videoToPlay";
+import AddComment from "../../components/Comment/AddComment/AddComment";
 
 const SingleVideo = ({ router }) => {
   console.log(router.query.videoDetail);
@@ -9,6 +10,7 @@ const SingleVideo = ({ router }) => {
     <Grid container spacing={2}>
       <Grid item xs={6}>
         <VideoToPlay />
+        <AddComment />
       </Grid>
       <Grid item xs={5}>
         chapters

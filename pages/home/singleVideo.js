3d33fac953ff79a5eee992e63956d3296c84d97c
Murad <murad.jaddoon@gmail.com>
2022-06-02 19:52:27 +0500

    updated single video page with videoToPlay comp

--- pages/home/singleVideo.js
+++ pages/home/singleVideo.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import { withRouter } from 'next/router'
+import React from "react";
+import { withRouter } from "next/router";
+import { Grid } from "@mui/material";
+import VideoToPlay from "../../components/Video/videoToPlay/videoToPlay";
 
-const SingleVideo = ({router}) => {
-  console.log(router.query)
+const SingleVideo = ({ router }) => {
+  console.log(router.query.videoDetail);
   return (
-    <h1>SingleVideo</h1>
-  )
-}
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <VideoToPlay />
+      </Grid>
+      <Grid item xs={5}>
+        chapters
+      </Grid>
+    </Grid>
+  );
+};
 
-export default withRouter(SingleVideo)
+export default withRouter(SingleVideo);

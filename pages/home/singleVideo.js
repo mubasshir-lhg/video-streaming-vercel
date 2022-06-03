4a80c60dd7454ffa27480d5fe1ca74206316ffe5
Murad <murad.jaddoon@gmail.com>
2022-06-03 14:22:34 +0500

    subscriber comments added to single video page

--- pages/home/singleVideo.js
+++ pages/home/singleVideo.js
@@ -3,6 +3,7 @@ import { withRouter } from "next/router";
 import { Grid } from "@mui/material";
 import VideoToPlay from "../../components/Video/videoToPlay/videoToPlay";
 import AddComment from "../../components/Comment/AddComment/AddComment";
+import SubscriberComment from "../../components/Comment/SubscriberComment/SubscriberComment";
 
 const SingleVideo = ({ router }) => {
   console.log(router.query.videoDetail);
@@ -11,6 +12,7 @@ const SingleVideo = ({ router }) => {
       <Grid item xs={6}>
         <VideoToPlay />
         <AddComment />
+        <SubscriberComment />
       </Grid>
       <Grid item xs={5}>
         chapters

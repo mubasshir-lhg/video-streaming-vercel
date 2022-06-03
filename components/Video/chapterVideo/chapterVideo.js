c80adea3f154e9d9450a2c95e029408ca4258ca3
Murad <murad.jaddoon@gmail.com>
2022-06-03 21:07:37 +0500

    chapter video comp created

---
+++ components/Video/chapterVideo/chapterVideo.js
@@ -0,0 +1,39 @@
+import { Typography, Box } from "@mui/material";
+import React from "react";
+import Video from "../Video";
+import { styled } from "@mui/system";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+
+const ChaptersWrapper = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: 50,
+  margin: "10px 30px",
+  cursor: "pointer",
+  position: "relative",
+});
+const PlayBtn = styled(Box)({
+  position: "absolute",
+  top: "translate(50%,-50%)",
+  left: -25,
+  transform: "scale(0)",
+});
+const ChapterVideo = ({ item, onClick, activeBorder, activePlayBtn }) => {
+  const { videoSrc, title, time } = item;
+  return (
+    <Box>
+      <ChaptersWrapper onClick={onClick}>
+        <PlayBtn sx={activePlayBtn}>
+          <PlayArrowIcon />
+        </PlayBtn>
+        <Video src={videoSrc} width="180px" my="0" />
+        <Box mr={4} sx={activeBorder} flexGrow={1}>
+          <Typography variant="subtitle1">{title}</Typography>
+          <Typography variant="caption">{time}</Typography>
+        </Box>
+      </ChaptersWrapper>
+    </Box>
+  );
+};
+
+export default ChapterVideo;

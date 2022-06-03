379d6d40811ff4c654972cfd4a0c852c28d9187f
Murad <murad.jaddoon@gmail.com>
2022-06-03 21:05:37 +0500

    chapters mockup created

---
+++ _mockup/Chapters.js
@@ -0,0 +1,26 @@
+export const chaptersData = [
+  {
+    id: 1,
+    videoSrc: "/video1.mp4",
+    title: "Intro",
+    time: "00:00",
+  },
+  {
+    id: 2,
+    videoSrc: "/video3.mp4",
+    title: "Specs",
+    time: "00:44",
+  },
+  {
+    id: 3,
+    videoSrc: "/video1.mp4",
+    title: "Review",
+    time: "4:21",
+  },
+  {
+    id: 4,
+    videoSrc: "/video3.mp4",
+    title: "Pricing",
+    time: "5:30",
+  },
+];

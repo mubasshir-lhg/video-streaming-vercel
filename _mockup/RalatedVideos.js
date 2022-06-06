c385944f9f200ce5121331a11f183c3afdfb7a02
Murad <murad.jaddoon@gmail.com>
2022-06-06 15:56:51 +0500

    related videos mockup created

---
+++ _mockup/RalatedVideos.js
@@ -0,0 +1,75 @@
+export const RelatedVideosData = [
+  {
+    id: 1,
+    videoSrc: "/video1.mp4",
+    title: "M1 Max MacBook Pro Review",
+    authorName: " John Doe Vlogs",
+    authorImg: "/user1.jpg",
+    views: "267k",
+    uploadTime: "3 months",
+    duration: "4:00",
+    description:
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+  },
+  {
+    id: 2,
+    videoSrc: "/video3.mp4",
+    title: "M1 Max MacBook Pro Review",
+    authorName: " Lucy Grey",
+    authorImg: "/user2.jpg",
+    views: "267k",
+    uploadTime: "3 months",
+    duration: "4:00",
+    description:
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+  },
+  {
+    id: 3,
+    videoSrc: "/video1.mp4",
+    title: "M1 Max MacBook Pro Review",
+    authorName: " John Doe Vlogs",
+    authorImg: "/user1.jpg",
+    views: "267k",
+    uploadTime: "3 months",
+    duration: "4:00",
+    description:
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+  },
+  {
+    id: 4,
+    videoSrc: "/video3.mp4",
+    title: "M1 Max MacBook Pro Review",
+    authorName: " Lucy Grey",
+    authorImg: "/user2.jpg",
+    views: "267k",
+    uploadTime: "3 months",
+    duration: "4:00",
+    description:
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+  },
+  {
+    id: 5,
+    videoSrc: "/video1.mp4",
+    title: "M1 Max MacBook Pro Review",
+    authorName: " John Doe Vlogs",
+    authorImg: "/user1.jpg",
+    views: "267k",
+    uploadTime: "3 months",
+    duration: "4:00",
+    description:
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+  },
+  {
+    id: 6,
+    videoSrc: "/video3.mp4",
+    title: "M1 Max MacBook Pro Review",
+    authorName: " Lucy Grey",
+    authorImg: "/user2.jpg",
+    views: "267k",
+    uploadTime: "3 months",
+    duration: "4:00",
+    description:
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+  },
+  
+];

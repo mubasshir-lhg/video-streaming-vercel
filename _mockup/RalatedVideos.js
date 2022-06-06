06a155bf2d85c56b677bfe886c479cf3888bca7d
Murad <murad.jaddoon@gmail.com>
2022-06-06 20:23:54 +0500

    updated related data mockup

--- _mockup/RalatedVideos.js
+++ _mockup/RalatedVideos.js
@@ -2,62 +2,62 @@ export const RelatedVideosData = [
   {
     id: 1,
     videoSrc: "/video1.mp4",
-    title: "M1 Max MacBook Pro Review",
+    title: "M1 Max MacBook Pro Review Nostrud tempor nostrud ad excepteur ipsum.",
     authorName: " John Doe Vlogs",
     authorImg: "/user1.jpg",
     views: "267k",
     uploadTime: "3 months",
     duration: "4:00",
     description:
-      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.Eu dolor duis nostrud eu magna voluptate.",
   },
   {
     id: 2,
     videoSrc: "/video3.mp4",
-    title: "M1 Max MacBook Pro Review",
+    title: "M1 Max MacBook Pro Review Dolore aliqua labore deserunt nostrud sit ut.",
     authorName: " Lucy Grey",
     authorImg: "/user2.jpg",
     views: "267k",
     uploadTime: "3 months",
     duration: "4:00",
     description:
-      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.Tempor laborum duis reprehenderit aliquip ea quis deserunt.",
   },
   {
     id: 3,
     videoSrc: "/video1.mp4",
-    title: "M1 Max MacBook Pro Review",
+    title: "M1 Max MacBook Pro Review Veniam elit qui officia consectetur cillum in.",
     authorName: " John Doe Vlogs",
     authorImg: "/user1.jpg",
     views: "267k",
     uploadTime: "3 months",
     duration: "4:00",
     description:
-      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.Laborum cillum voluptate minim deserunt.",
   },
   {
     id: 4,
     videoSrc: "/video3.mp4",
-    title: "M1 Max MacBook Pro Review",
+    title: "M1 Max MacBook Pro Review Sunt reprehenderit nostrud ullamco do irure Lorem ex labore laboris.",
     authorName: " Lucy Grey",
     authorImg: "/user2.jpg",
     views: "267k",
     uploadTime: "3 months",
     duration: "4:00",
     description:
-      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.Irure veniam aute esse culpa.",
   },
   {
     id: 5,
     videoSrc: "/video1.mp4",
-    title: "M1 Max MacBook Pro Review",
+    title: "M1 Max MacBook Pro Review Est est ullamco adipisicing mollit duis exercitation ipsum ea reprehenderit fugiat cillum.",
     authorName: " John Doe Vlogs",
     authorImg: "/user1.jpg",
     views: "267k",
     uploadTime: "3 months",
     duration: "4:00",
     description:
-      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.Ipsum amet est magna velit ad tempor quis cillum dolor sint minim.",
   },
   {
     id: 6,
@@ -69,7 +69,7 @@ export const RelatedVideosData = [
     uploadTime: "3 months",
     duration: "4:00",
     description:
-      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.",
+      "Sit reprehenderit velit laboris qui qui.Do sunt laboris sunt mollit tempor.Sit dolore irure mollit aliqua occaecat ipsum id.",
   },
   
 ];

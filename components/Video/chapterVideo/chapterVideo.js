358c856040e2c00397fe7a0b648b90e121d730cc
Murad <murad.jaddoon@gmail.com>
2022-06-07 16:25:02 +0500

    make single video page fully responsive

--- components/Video/chapterVideo/chapterVideo.js
+++ components/Video/chapterVideo/chapterVideo.js
@@ -4,16 +4,22 @@ import Video from "../Video";
 import { styled } from "@mui/system";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
-const ChaptersWrapper = styled(Box)(({theme})=>({
+const ChaptersWrapper = styled(Box)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   margin: "0 30px",
-  [theme.breakpoints.down("lg")]:{
+  [theme.breakpoints.down("lg")]: {
     margin: "0 0 0 30px",
   },
   padding: "10px 0",
   cursor: "pointer",
   position: "relative",
+}));
+const VideoWrapper = styled(Box)(({ theme }) => ({
+  width: "180px",
+  [theme.breakpoints.down("sm")]: {
+    width: "130px",
+  },
 }));
 const PlayBtn = styled(Box)({
   position: "absolute",
@@ -35,7 +41,9 @@ const ChapterVideo = ({ item, onClick, activeChapter, lastChapter }) => {
         <PlayBtn sx={{ transform: activeChapter && "scale(1)" }}>
           <PlayArrowIcon />
         </PlayBtn>
-        <Video src={videoSrc} width="180px" my="0" />
+        <VideoWrapper>
+          <Video src={videoSrc} width="100%" my="0" />
+        </VideoWrapper>
         <Box
           sx={{ borderRight: activeChapter && "2px solid #fff" }}
           flexGrow={1}

26428e296a1a40e623dbd05cd77bebad20e1625e
Murad <murad.jaddoon@gmail.com>
2022-06-14 18:46:19 +0500

    updated login page

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,35 +1,36 @@
 import React from "react";
-import { Grid,Box,TextField, Button } from "@mui/material";
+import { Grid, Box, TextField, Button } from "@mui/material";
 import BoxContainer from "../../components/BoxContainer/BoxContainer";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import logo from "../../assets/Images/logo/Teachmetoo Brandmark Full Color .png";
 
-
 const Label = ({ children }) => (
-    <Box sx={{typography: {xs:"subtitle1",md:"subtitle2"} }}>{children}</Box>
-  );
+  <Box sx={{ typography: { xs: "subtitle1", md: "subtitle2" } }}>
+    {children}
+  </Box>
+);
 const LoginPage = () => {
-    const router=useRouter()
-    const loginHandler=()=>{
-        router.push("/home/")
-    }
+  const router = useRouter();
+  const loginHandler = () => {
+    router.push("/home/");
+  };
   return (
     <Grid container justifyContent="center" mt={15}>
       <Grid item xs={12} sm={8} md={6} lg={4} xl={3}>
         <BoxContainer bgColor="primary.light">
-          <Box sx={{typography:"h4",textAlign:"center"}}>
-          <Image
-                alt="logo"
-                src={logo}
-                width={40}
-                height={40}
-                quality={100}
-                objectFit="cover"
-              />
+          <Box sx={{ typography: "h4", textAlign: "center" }}>
+            <Image
+              alt="logo"
+              src={logo}
+              width={40}
+              height={40}
+              quality={100}
+              objectFit="cover"
+            />
           </Box>
-          <Box sx={{typography:"h4",textAlign:"center"}}>Login</Box>
+          <Box sx={{ typography: "h4", textAlign: "center" }}>Login</Box>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <Label>Email</Label>
@@ -37,13 +38,20 @@ const LoginPage = () => {
             </Grid>
             <Grid item xs={12}>
               <Label>Password</Label>
-              <TextField name="password" type="password" size="small"  />
+              <TextField name="password" type="password" size="small" />
             </Grid>
             <Grid item xs={12}>
-             <Button variant="contained" color="warning" fullWidth onClick={loginHandler}>Login</Button>
+              <Button
+                variant="contained"
+                color="warning"
+                fullWidth
+                onClick={loginHandler}
+              >
+                Login
+              </Button>
             </Grid>
             <Grid item xs={12} textAlign="right">
-                <Link href="/auth/signup">don&apos;t have account? Signup</Link>
+              <Link href="/auth/signup">don&apos;t have account? Signup</Link>
             </Grid>
           </Grid>
         </BoxContainer>

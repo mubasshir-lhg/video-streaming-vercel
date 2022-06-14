c57c897c3ab0f56acefd1d0b1afb9ec89dbf322f
Murad <murad.jaddoon@gmail.com>
2022-06-14 14:43:50 +0500

    created notification mockup

---
+++ _mockup/AppBar/Notification.js
@@ -0,0 +1,26 @@
+export const NotificationData = [
+  {
+    senderImg: "/user1.jpg",
+    senderName: "John Doe",
+    rectime: "2 minuts ago",
+    senderText: "Velit magna irure do ea duis dolore ut labore.",
+  },
+  {
+    senderImg: "/user2.jpg",
+    senderName: "Lucy",
+    rectime: "2 minuts ago",
+    senderText: "Velit magna irure do ea duis dolore ut labore.",
+  },
+  {
+    senderImg: "/user1.jpg",
+    senderName: "John Doe",
+    rectime: "2 minuts ago",
+    senderText: "Velit magna irure do ea duis dolore ut labore.",
+  },
+  {
+    senderImg: "/user2.jpg",
+    senderName: "Lucy",
+    rectime: "2 minuts ago",
+    senderText: "Velit magna irure do ea duis dolore ut labore.",
+  },
+];

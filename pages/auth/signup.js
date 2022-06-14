14fd9a009f22b90df0e8e7fe64ebd0f09dc7521e
Murad <murad.jaddoon@gmail.com>
2022-06-14 17:30:12 +0500

    updated color

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,48 +1,55 @@
 import React from "react";
-import { Grid,Box,TextField, Button } from "@mui/material";
-import BoxContainer from "../../components/BoxContainer/BoxContainer";
-import LockOpenIcon from '@mui/icons-material/LockOpen';
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
+import { Grid, Box, TextField, Button } from "@mui/material";
+import BoxContainer from "../../components/BoxContainer/BoxContainer";
 import logo from "../../assets/Images/logo/Teachmetoo Brandmark Full Color .png";
 
 const Label = ({ children }) => (
-    <Box sx={{ pb: 1, typography: {xs:"subtitle1",md:"subtitle2"} }}>{children}</Box>
-  );
+  <Box sx={{ typography: { xs: "subtitle1", md: "subtitle2" } }}>
+    {children}
+  </Box>
+);
 const SignupPage = () => {
   return (
-    <Grid container justifyContent="center" mt={6}>
+    <Grid container justifyContent="center" mt={15}>
       <Grid item xs={12} sm={8} md={6} lg={4} xl={3}>
-        <BoxContainer>
-          <Box sx={{typography:"h4",textAlign:"center"}}>
-          <Image
-                alt="logo"
-                src={logo}
-                width={40}
-                height={40}
-                quality={100}
-                objectFit="cover"
-              />
+        <BoxContainer bgColor="primary.light">
+          <Box sx={{ typography: "h4", textAlign: "center" }}>
+            <Image
+              alt="logo"
+              src={logo}
+              width={40}
+              height={40}
+              quality={100}
+              objectFit="cover"
+            />
           </Box>
-          <Box sx={{typography:"h4",textAlign:"center"}}>Signup</Box>
+          <Box sx={{ typography: "h4", textAlign: "center" }}>Signup</Box>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <Label>Name</Label>
-              <TextField name="name" type="text"/>
+              <TextField name="name" type="text" size="small" />
             </Grid>
             <Grid item xs={12}>
               <Label>Email</Label>
-              <TextField name="title" type="text"/>
+              <TextField name="title" type="text" size="small" />
             </Grid>
             <Grid item xs={12}>
               <Label>Password</Label>
-              <TextField name="password" type="password" />
+              <TextField name="password" type="password" size="small" />
             </Grid>
             <Grid item xs={12}>
-             <Button variant="contained" color="warning" fullWidth >Signup</Button>
+              <Label>Confirm Password</Label>
+              <TextField name="confirm-password" type="password" size="small" />
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained" color="warning" fullWidth>
+                Signup
+              </Button>
             </Grid>
             <Grid item xs={12} textAlign="right">
-                <Link href="/auth/login">already have account? login</Link>
+              <Link href="/auth/login">already have account? login</Link>
             </Grid>
           </Grid>
         </BoxContainer>

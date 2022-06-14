cbdaba50d05ea0dfdc128a6a85de310965e136af
Murad <murad.jaddoon@gmail.com>
2022-06-14 15:32:57 +0500

    fix max height of popup

--- components/PopupMenu/PopupMenu.js
+++ components/PopupMenu/PopupMenu.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Menu, MenuItem } from "@mui/material";
 
-const PopupMenu = ({  anchorEl, open, onClose, options }) => {
+const PopupMenu = ({ anchorEl, open, onClose, options }) => {
   return (
     <Menu
       anchorEl={anchorEl}
       open={open}
       onClose={onClose}
       disableScrollLock={true}
+      sx={{ maxHeight: "400px" }}
     >
-       {options?.map((item, index) => (
-          <MenuItem onClick={item.onClickFunc} key={index}>
-            {item.child}
-          </MenuItem>
-        ))}
+      {options?.map((item, index) => (
+        <MenuItem onClick={item.onClickFunc} key={index}>
+          {item.child}
+        </MenuItem>
+      ))}
     </Menu>
   );
 };

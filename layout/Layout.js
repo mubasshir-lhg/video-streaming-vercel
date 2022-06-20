73c1f65dbc68f0206be6ccc39e756bb3c9ccba68
Murad <murad.jaddoon@gmail.com>
2022-06-20 15:54:12 +0500

    updated layout

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,40 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { styled, useTheme } from "@mui/material/styles";
 import { useRouter } from "next/router";
-//data
-import { NotificationData } from "../_mockup/AppBar/Notification";
-//components
-import SearchField from "../components/SearchField/SearchField";
-import { RequestModal } from "../components/Modal/RequestAVideo/RequestAVideo";
-import { UploadVideoModal } from "../components/Modal/UploadVideoModal/UploadVideoModal";
-import PopupMenu from "../components/PopupMenu/PopupMenu";
-import NotificationCard from "../components/Cards/NotificationCard/NotificationCard";
 import Head from "next/head";
-//images
 import Image from "next/image";
 import dp from "../assets/Images/user1.jpg";
-import logo from "../assets/Images/logo/Teachmetoo Brandmark Full Color .png";
-//icons
+
 import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import PersonAddOutlinedIcon from "@mui/icons-material/PersonAddOutlined";
 import ModeCommentOutlinedIcon from "@mui/icons-material/ModeCommentOutlined";
 import SubtitlesOutlinedIcon from "@mui/icons-material/SubtitlesOutlined";
-import VideoCallOutlinedIcon from "@mui/icons-material/VideoCallOutlined";
 import VideoCallIcon from "@mui/icons-material/VideoCall";
-import VideocamOutlinedIcon from "@mui/icons-material/VideocamOutlined";
-import MicNoneOutlinedIcon from "@mui/icons-material/MicNoneOutlined";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import MenuIcon from "@mui/icons-material/Menu";
+
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import HomeIcon from "@mui/icons-material/Home";
 import FlipCameraAndroidIcon from "@mui/icons-material/FlipCameraAndroid";
 import HistoryIcon from "@mui/icons-material/History";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import Badge from "@mui/material/Badge";
-//mui
+import AppBarComp from "../components/AppBar/AppBar";
+
 import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar from "@mui/material/AppBar";
 import {
   useMediaQuery,
   ListItemButton,
@@ -43,7 +27,6 @@ import {
   CssBaseline,
   IconButton,
   Divider,
-  Toolbar,
   List,
   Box,
 } from "@mui/material";
@@ -140,24 +123,6 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth + 24}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
 const Drawer = styled(MuiDrawer, {
   shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
@@ -179,149 +144,16 @@ export default function Layout({ children }) {
   const router = useRouter();
   const { breakpoints } = useTheme();
   const [open, setOpen] = useState(true);
-  const [openRequestModal, setOpenRequestModal] = useState(false);
-  const [openUploadModal, setOpenUploadModal] = useState(false);
   const [activeIndex, setActiveIndex] = useState(0);
-  const [navbar, setNavbar] = useState(false);
-  const [anchorEl, setAnchorEl] = useState(null);
   const [channel, setChannel] = useState(false);
-  const [menuOption, setMenuOption] = useState([]);
-  const isMdDown = useMediaQuery(breakpoints.down("md"));
   const isSmDown = useMediaQuery(breakpoints.down("sm"));
 
-  const getSearchValue = (searchVal) => {
-    router.push(
-      {
-        pathname: "/home/searchResult",
-        query: { searchVal: searchVal },
-      },
-      "/home/searchResult"
-    );
-  };
-  const logoHandler = () => {
-    router.push("/home/");
-    setChannel(false);
-  };
-  const uploadVideo = () => {
-    router.push("/home/uploadVideo/splitChapters");
-    handleCloseModal();
-  };
-  const openPopup = Boolean(anchorEl);
-  const openPopupMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const closePopupMenu = () => {
-    setAnchorEl(null);
-  };
-  const switchToChannel = () => {
-    setChannel(true);
-    closePopupMenu();
-    router.push("/dashboard/");
-  };
-  const switchToHome = () => {
-    setChannel(false);
-    closePopupMenu();
-    router.push("/home/");
-  };
   const handleDrawer = () => {
     setOpen((preState) => !preState);
-  };
-  const handleCloseModal = () => {
-    setOpenRequestModal(false);
-    setOpenUploadModal(false);
   };
   const goToPage = (href, index) => {
     channel ? router.push(`/dashboard/${href}`) : router.push(`/home/${href}`);
     setActiveIndex(index);
-  };
-  const logout = () => {
-    router.push(`/auth/login`);
-    closePopupMenu();
-  };
-  const goToProfile = () => {
-    router.push(`/dashboard/profile`);
-    closePopupMenu();
-    setChannel(true);
-  };
-  const notificationHandler = () => {};
-  const changeBackground = () => {
-    if (window.scrollY >= 10) {
-      setNavbar(true);
-    } else {
-      setNavbar(false);
-    }
-  };
-
-  useEffect(() => {
-    if (isMdDown) {
-      setOpen(false);
-    } else {
-      setOpen(true);
-    }
-    changeBackground();
-    window.addEventListener("scroll", changeBackground);
-  }, [isMdDown]);
-
-  const menuOptions1 = [
-    { onClickFunc: switchToChannel, child: "Your Channel" },
-    { onClickFunc: switchToHome, child: "Home" },
-    { onClickFunc: goToProfile, child: "Profile" },
-    { onClickFunc: logout, child: "Logout" },
-  ];
-  const menuOptions2 = [
-    {
-      onClickFunc: () => {
-        setOpenRequestModal(true);
-        closePopupMenu();
-      },
-      child: (
-        <>
-          <VideoCallOutlinedIcon sx={{ pr: 1 }} />
-          Request Video
-        </>
-      ),
-    },
-    {
-      onClickFunc: closePopupMenu,
-      child: (
-        <>
-          <MicNoneOutlinedIcon sx={{ pr: 1 }} />
-          Voice
-        </>
-      ),
-    },
-    {
-      onClickFunc: () => {
-        setOpenUploadModal(true);
-        closePopupMenu();
-      },
-      child: (
-        <>
-          <VideocamOutlinedIcon sx={{ pr: 1 }} />
-          Upload video
-        </>
-      ),
-    },
-    {
-      onClickFunc: closePopupMenu,
-      child: (
-        <>
-          <NotificationsOutlinedIcon sx={{ pr: 1 }} />
-          Notification
-        </>
-      ),
-    },
-  ];
-
-  const notificationOption = NotificationData?.map((item, index) => {
-    return {
-      onClickFunc: closePopupMenu,
-      child: <NotificationCard item={item} />,
-    };
-  });
-  const updateMenuOptions = (e, opt) => {
-    setMenuOption(opt);
-    openPopupMenu(e);
   };
 
   return (
@@ -331,97 +163,14 @@ export default function Layout({ children }) {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <CssBaseline />
-      <AppBar
-        position="fixed"
+      <AppBarComp
         open={open}
-        sx={{
-          py: 2,
-          backgroundColor: navbar && "#2f3566",
-        }}
-      >
-        <Toolbar>
-          <>
-            <IconButton
-              onClick={handleDrawer}
-              edge="start"
-              sx={{
-                mr: { xs: 0, sm: 3 },
-                ml: { xs: 0, sm: 2 },
-                ...(open && { display: "none" }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Box
-              mt={1}
-              sx={{ flexGrow: 1, minWidth: "25px", cursor: "pointer" }}
-              onClick={logoHandler}
-            >
-              <Image
-                alt="logo"
-                src={logo}
-                width={40}
-                height={40}
-                quality={100}
-                objectFit="cover"
-              />
-            </Box>
-            <Box sx={{ flexGrow: 1 }}>
-              <SearchField getSearchValue={getSearchValue} />
-            </Box>
-            {isSmDown ? (
-              <IconButton onClick={(e) => updateMenuOptions(e, menuOptions2)}>
-                <MoreVertIcon />
-              </IconButton>
-            ) : (
-              <>
-                <IconButton onClick={() => setOpenRequestModal(true)}>
-                  <VideoCallOutlinedIcon />
-                </IconButton>
-                <IconButton>
-                  <MicNoneOutlinedIcon />
-                </IconButton>
-                <IconButton onClick={() => setOpenUploadModal(true)}>
-                  <VideocamOutlinedIcon />
-                </IconButton>
-                <IconButton
-                  onClick={(e) => updateMenuOptions(e, notificationOption)}
-                  sx={{ mr: 1 }}
-                >
-                  <Badge badgeContent={notificationOption.length} color="error">
-                    <NotificationsOutlinedIcon />
-                  </Badge>
-                </IconButton>
-              </>
-            )}
-            <Box
-              mt={1}
-              sx={{ minWidth: "25px", cursor: "pointer" }}
-              id="basic-button"
-              aria-controls={openPopup ? "basic-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={openPopup ? "true" : undefined}
-              onClick={(e) => updateMenuOptions(e, menuOptions1)}
-            >
-              <Image
-                alt="Dp"
-                src={dp}
-                width={30}
-                height={30}
-                quality={100}
-                objectFit="cover"
-                className="bar-img"
-              />
-            </Box>
-            <PopupMenu
-              anchorEl={anchorEl}
-              open={openPopup}
-              onClose={closePopupMenu}
-              options={menuOption}
-            />
-          </>
-        </Toolbar>
-      </AppBar>
+        handleDrawer={handleDrawer}
+        isSmDown={isSmDown}
+        drawerWidth={drawerWidth}
+        setOpen={setOpen}
+        setChannel={setChannel}
+      />
       <Drawer
         variant="permanent"
         open={open}
@@ -486,13 +235,6 @@ export default function Layout({ children }) {
         <DrawerHeader />
         {children}
       </Box>
-
-      <RequestModal open={openRequestModal} handleClose={handleCloseModal} />
-      <UploadVideoModal
-        open={openUploadModal}
-        handleClose={handleCloseModal}
-        onClick={uploadVideo}
-      />
     </Box>
   );
 }

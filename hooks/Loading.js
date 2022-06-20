573466a6fd231816a81290dcc16e7160102bac42
Murad <murad.jaddoon@gmail.com>
2022-06-20 20:51:33 +0500

    created custome hook for loading

---
+++ hooks/Loading.js
@@ -0,0 +1,26 @@
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+// import NProgress from "nprogress"; //nprogress module
+// import "nprogress/nprogress.css"; //styles of nprogress
+// NProgress.start()
+//  NProgress.done()
+function useLoading() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => {
+      setLoading(true);
+    };
+    const handleComplete = () => {
+      setLoading(false);
+    };
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [loading, router.events]);
+
+  return loading;
+}
+
+export default useLoading;

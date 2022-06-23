dd4bd7e51c2f699fec0d455bde3e466919aba504
muhammad affan <affanmuhammad623@gmail.com>
2022-06-23 20:23:24 +0500

    loginWithToken

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,39 @@ import AuthLayout from "../layout/AuthLayout";
 import Axios from "axios"
 import { UserContext } from "../context/userContext";
 import { useState } from "react";
-
+import {loginWithToken,getAccessToken,logout} from "../services/auth-services"
+import {useEffect} from "react";
 Axios.defaults.baseURL="http://localhost:80/"
 
 function MyApp({ Component, pageProps, router }) {
   const[user,setUser] = useState(null);
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const checkAuth = () => {
+    const token = getAccessToken();
+    if (token !== null) {
+      loginWithToken(token)
+        .then((res) => {
+          console.log(res);
+          if (res?.data) {
+            setUser(res?.data?.user);
+            router.push("/home/")
+          } else {
+            setUser(null);
+            logout();
+          }
+        })
+        .catch((err) => {
+          setUser(null);
+          logout();
+        });
+    } else {
+      setUser(null);
+    }
+  };
+
   return (
     <UserContext.Provider value={{user,setUser}} >
     <ThemeConfig>

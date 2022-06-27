e97e1490b5c789c812267cf2bb18f3c60449ca21
Murad <murad.jaddoon@gmail.com>
2022-06-27 15:06:33 +0500

    updated channel profile tabs

--- pages/home/channelProfile/index.js
+++ pages/home/channelProfile/index.js
@@ -1,35 +1,42 @@
 import { Grid, Box, Button, Typography } from "@mui/material";
 import { styled } from "@mui/system";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import banner from "../../../assets/Images/banner.jpg";
 import Image from "next/image";
 import ChannelProfileCard from "../../../components/Cards/ChannelProfileCard/ChannelProfileCard";
 
-const tabs = ["HOME,VIDEOS,PLAYLIST,ABOUT"];
-const LinksWrapper = styled(Box)(({ theme }) => ({
+const tabs = ["All", "Recent", "Popular"];
+const TabsWrapper = styled(Box)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "flex-start",
+  marginTop:"8px",
   gap: "50px",
-  marginBottom: "20px",
   [theme.breakpoints.down("sm")]: {
     gap: "16px",
     flexWrap: "wrap",
   },
 }));
+const FlexBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
 const ChannelProfile = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  };
   return (
     <Grid container>
-      <Grid xs={12}>
-        <Image
-          src={banner}
-          alt="banner"
-          className="w-100"
-          width={1800}
-          height={300}
-        />
+      <Grid
+        xs={12}
+        sx={{
+          "& span": { height: "100% !important", width: "100% !important" },
+        }}
+      >
+        <Image src={banner} alt="banner" height={220} />
       </Grid>
       <Grid xs={12}>
         <Box
@@ -39,34 +46,26 @@ const ChannelProfile = () => {
             py: 3,
           }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
+          <FlexBox>
             <ChannelProfileCard />
             <Button variant="contained" color="warning">
               SUBSCRIBE
             </Button>
-          </Box>
-          <Box sx={{ mt: 2 }}>
-            <LinksWrapper>
-              {tabs?.map((item, index) => (
-                <Box
-                  key={index}
-                  sx={{
-                    cursor: "pointer",
-                    borderBottom: activeIndex === index && "1px solid #fff",
-                  }}
-                  onClick={() => handleClick(index)}
-                >
-                  <Typography variant="subtitle1"> {item}</Typography>
-                </Box>
-              ))}
-            </LinksWrapper>
-          </Box>
+          </FlexBox>
+          <TabsWrapper>
+            {tabs?.map((item, index) => (
+              <Box
+                key={index}
+                sx={{
+                  cursor: "pointer",
+                  borderBottom: activeIndex === index && "1px solid #fff",
+                }}
+                onClick={() => handleClick(index)}
+              >
+                <Typography variant="subtitle1"> {item}</Typography>
+              </Box>
+            ))}
+          </TabsWrapper>
         </Box>
       </Grid>
       <Grid xs={12}>Recent video</Grid>

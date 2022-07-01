1a32eda352e1ee60084efc44a9fa019f5512e02b
Murad <murad.jaddoon@gmail.com>
2022-07-01 20:39:00 +0500

    updated home mockup

--- _mockup/Home.js
+++ _mockup/Home.js
@@ -1,7 +1,7 @@
 export const homedata=[
     {
         id:1,
-        videoSrc:'video1.mp4',
+        videoSrc:'/video1.mp4',
         authorImg:'user1.jpg',
         title:'Veniam Arts do do et dolor nostrud sit Lorem.',
         authorName:'John Doe Vlogs',
@@ -12,7 +12,7 @@ export const homedata=[
     },
     {
         id:2,
-        videoSrc:'video3.mp4',
+        videoSrc:'/video3.mp4',
         authorImg:'user2.jpg',
         title:'Veniam Crafts do do et dolor nostrud sit Lorem.',
         authorName:'Lucy Gray Vlogs',
@@ -23,7 +23,7 @@ export const homedata=[
     },
     {
         id:3,
-        videoSrc:'video1.mp4',
+        videoSrc:'/video1.mp4',
         authorImg:'user1.jpg',
         title:'Veniam daily hacks do do et dolor nostrud sit Lorem.',
         authorName:'John Doe Vlogs',
@@ -34,7 +34,7 @@ export const homedata=[
     },
     {
         id:4,
-        videoSrc:'video3.mp4',
+        videoSrc:'/video3.mp4',
         authorImg:'user2.jpg',
         title:'Veniam minim do do et dolor nostrud sit Lorem.',
         authorName:'Lucy Gray Vlogs',
@@ -45,7 +45,7 @@ export const homedata=[
     },
     {
         id:5,
-        videoSrc:'video1.mp4',
+        videoSrc:'/video1.mp4',
         authorImg:'user1.jpg',
         title:'Veniam minim do do et dolor nostrud sit Lorem.',
         authorName:'John Doe Vlogs',
@@ -56,7 +56,7 @@ export const homedata=[
     },
     {
         id:6,
-        videoSrc:'video3.mp4',
+        videoSrc:'/video3.mp4',
         authorImg:'user2.jpg',
         title:'Veniam minim do do et dolor nostrud sit Lorem.',
         authorName:'Lucy Gray Vlogs',
@@ -67,7 +67,7 @@ export const homedata=[
     },
     {
         id:7,
-        videoSrc:'video1.mp4',
+        videoSrc:'/video1.mp4',
         authorImg:'user1.jpg',
         title:'Veniam minim do do et dolor nostrud sit Lorem.',
         authorName:'John Doe Vlogs',
@@ -79,7 +79,97 @@ export const homedata=[
     },
     {
         id:8,
-        videoSrc:'video3.mp4',
+        videoSrc:'/video3.mp4',
+        authorImg:'user2.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'Lucy Gray Vlogs',
+        views:'569k',
+        uploadTime:'4months',
+        duration:'4:00',
+        category:'music'
+
+    },
+    {
+        id:1,
+        videoSrc:'/video1.mp4',
+        authorImg:'user1.jpg',
+        title:'Veniam Arts do do et dolor nostrud sit Lorem.',
+        authorName:'John Doe Vlogs',
+        views:'267k',
+        uploadTime:'3months',
+        duration:'4:00',
+        category:'arts'
+    },
+    {
+        id:2,
+        videoSrc:'/video3.mp4',
+        authorImg:'user2.jpg',
+        title:'Veniam Crafts do do et dolor nostrud sit Lorem.',
+        authorName:'Lucy Gray Vlogs',
+        views:'569k',
+        uploadTime:'4months',
+        duration:'4:00',
+        category:'crafts'
+    },
+    {
+        id:3,
+        videoSrc:'/video1.mp4',
+        authorImg:'user1.jpg',
+        title:'Veniam daily hacks do do et dolor nostrud sit Lorem.',
+        authorName:'John Doe Vlogs',
+        views:'267k',
+        uploadTime:'3months',
+        duration:'4:00',
+        category:'arts'
+    },
+    {
+        id:4,
+        videoSrc:'/video3.mp4',
+        authorImg:'user2.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'Lucy Gray Vlogs',
+        views:'569k',
+        uploadTime:'4months',
+        duration:'4:00',
+        category:'crafts'
+    },
+    {
+        id:5,
+        videoSrc:'/video1.mp4',
+        authorImg:'user1.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'John Doe Vlogs',
+        views:'267k',
+        uploadTime:'3months',
+        duration:'4:00',
+        category:'daily hacks'
+    },
+    {
+        id:6,
+        videoSrc:'/video3.mp4',
+        authorImg:'user2.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'Lucy Gray Vlogs',
+        views:'569k',
+        uploadTime:'4months',
+        duration:'4:00',
+        category:'handmade'
+    },
+    {
+        id:7,
+        videoSrc:'/video1.mp4',
+        authorImg:'user1.jpg',
+        title:'Veniam minim do do et dolor nostrud sit Lorem.',
+        authorName:'John Doe Vlogs',
+        views:'267k',
+        uploadTime:'3months',
+        duration:'4:00',
+        category:'gardening'
+
+    },
+    {
+        id:8,
+        videoSrc:'/video3.mp4',
         authorImg:'user2.jpg',
         title:'Veniam minim do do et dolor nostrud sit Lorem.',
         authorName:'Lucy Gray Vlogs',

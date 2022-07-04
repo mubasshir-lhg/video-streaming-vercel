5b3a73f40ee8ed1535b759c3bdb109c67937179c
Murad <murad.jaddoon@gmail.com>
2022-07-04 20:10:58 +0500

    created watch history page

---
+++ pages/home/history/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Grid, Button, Typography } from "@mui/material";
+import { useRouter } from "next/router";
+import { RelatedVideosData } from "../../../_mockup/RalatedVideos";
+import SearchVideoResult from "../../../components/Video/searchVideoResult/searchVideoResult";
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const History = () => {
+  const router = useRouter();
+  const [data, setData] = useState(RelatedVideosData);
+
+  const playVideo = () => {
+    router.push("/home/playVideo");
+  };
+  return (
+    <Grid container spacing={2} mt={2}>
+      <Grid xs={12} px={2}>
+        <Typography variant="subtitle1">Watch history</Typography>
+      </Grid>
+      <Grid item xs={8}>
+        {data?.map((item, index) => (
+          <Grid item xs={12} key={index} sx={{ mt: 1, pr: 2 }}>
+            <SearchVideoResult
+              item={item}
+              onClick={playVideo}
+              hideAvatar={true}
+              hideDescription={true}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <Grid item xs={4} sx={{ backgroundColor: "primary.main" }}>
+        <Button variant="outlined" color='error' startIcon={<DeleteIcon />}>
+          Clear all watch history
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default History;

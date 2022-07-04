53517281637a55a3570868f4336a81371637d645
Murad <murad.jaddoon@gmail.com>
2022-07-04 18:55:15 +0500

    added recent and popluar videos to channel profile

--- pages/home/channelProfile/index.js
+++ pages/home/channelProfile/index.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { styled } from "@mui/system";
 import { homedata } from "../../../_mockup/Home";
 import { Grid, Box, Button, Typography, IconButton } from "@mui/material";
-import banner from "../../../assets/Images/banner.jpg";
+import { RequestModal } from "../../../components/Modal/RequestAVideo/RequestAVideo";
 import Image from "next/image";
 import Video from "../../../components/Video/Video";
-import ChannelProfileCard from "../../../components/Cards/ChannelProfileCard/ChannelProfileCard";
+import banner from "../../../assets/Images/banner.jpg";
 import HomeCard from "../../../components/Cards/HomeCard/HomeCard";
 import VideoCallOutlinedIcon from "@mui/icons-material/VideoCallOutlined";
-import { RequestModal } from "../../../components/Modal/RequestAVideo/RequestAVideo";
+import ChannelProfileCard from "../../../components/Cards/ChannelProfileCard/ChannelProfileCard";
 
 const tabs = ["All", "Recent", "Popular"];
 const TabsWrapper = styled(Box)(({ theme }) => ({
@@ -37,77 +37,107 @@ const ChannelProfile = () => {
   const handleCloseModal = () => {
     setOpenRequestModal(false);
   };
-  return (
-    <Grid container>
-      <Grid
-        item
-        xs={12}
-        sx={{
-          "& span": { height: "100% !important", width: "100% !important" },
-        }}
-      >
-        <Image src={banner} alt="banner" height={220} />
-      </Grid>
-      <Grid item xs={12}>
-        <Box
-          sx={{
-            bgcolor: "secondary.dark",
-            px: 4,
-            py: 3,
-          }}
-        >
-          <FlexBox>
-            <ChannelProfileCard />
-            <IconButton onClick={() => setOpenRequestModal(true)}>
-              <VideoCallOutlinedIcon fontSize='large'/>
-            </IconButton>
-            <Button variant="contained" color="warning">
-              SUBSCRIBE
-            </Button>
-          </FlexBox>
-          <TabsWrapper>
-            {tabs?.map((item, index) => (
-              <Box
-                key={index}
-                sx={{
-                  cursor: "pointer",
-                  borderBottom: activeIndex === index && "1px solid #fff",
-                }}
-                onClick={() => handleClick(index)}
-              >
-                <Typography variant="subtitle1"> {item}</Typography>
-              </Box>
-            ))}
-          </TabsWrapper>
-        </Box>
-      </Grid>
-      <Grid item xs={12} container gap={4} my={4}>
-        <Grid item xs={4}>
-          <Video src="/video1.mp4" width="100%" my="0" />
-        </Grid>
-        <Grid item xs={5}>
-          <Typography variant="h5">Most View Video</Typography>
-          <Typography variant="caption">234k views . 3 months ago</Typography>
-          <Typography variant="subtitle2" my={2}>
-            Laborum mollit in aliqua reprehenderit irure nulla ut aute.Veniam
-            consequat non ad officia ut ut minim cupidatat nostrud non non
-            tempor voluptate.
-          </Typography>
-          <Typography variant="body2">
-            Veniam consequat non ad officia ut ut minim cupidatat nostrud non
-            non tempor voluptate.
-          </Typography>
-        </Grid>
-      </Grid>
-      <Grid item xs={12} container spacing={2}>
-        {homedata?.map((item, index) => (
+  const compToShow = () => {
+    switch (activeIndex) {
+      case 1: //recent video
+        return homedata?.map((item, index) => (
           <Grid item xs={2} key={index}>
             <HomeCard channelProfile={true} item={item} />
           </Grid>
-        ))}
+        ));
+      case 2: // popula videos
+        return homedata?.map((item, index) => (
+          <Grid item xs={2} key={index}>
+            <HomeCard channelProfile={true} item={item} />
+          </Grid>
+        ));
+
+      default: // all videos
+        return (
+          <>
+            <Grid item xs={12} container gap={4} my={4}>
+              <Grid item xs={4}>
+                <Video src="/video1.mp4" width="100%" my="0" />
+              </Grid>
+              <Grid item xs={5}>
+                <Typography variant="h5">Most View Video</Typography>
+                <Typography variant="caption">
+                  234k views . 3 months ago
+                </Typography>
+                <Typography variant="subtitle2" my={2}>
+                  Laborum mollit in aliqua reprehenderit irure nulla ut
+                  aute.Veniam consequat non ad officia ut ut minim cupidatat
+                  nostrud non non tempor voluptate.
+                </Typography>
+                <Typography variant="body2">
+                  Veniam consequat non ad officia ut ut minim cupidatat nostrud
+                  non non tempor voluptate.
+                </Typography>
+              </Grid>
+            </Grid>
+            {homedata?.map((item, index) => (
+              <Grid item xs={2} key={index}>
+                <HomeCard channelProfile={true} item={item} />
+              </Grid>
+            ))}
+          </>
+        );
+    }
+  };
+  return (
+    <>
+      <Grid container>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            "& span": {
+              height: "100% !important",
+              width: "100% !important",
+            },
+          }}
+        >
+          <Image src={banner} alt="banner" height={220} />
+        </Grid>
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              bgcolor: "secondary.dark",
+              px: 4,
+              py: 3,
+            }}
+          >
+            <FlexBox>
+              <ChannelProfileCard />
+              <IconButton onClick={() => setOpenRequestModal(true)}>
+                <VideoCallOutlinedIcon fontSize="large" />
+              </IconButton>
+              <Button variant="contained" color="warning">
+                SUBSCRIBE
+              </Button>
+            </FlexBox>
+            <TabsWrapper>
+              {tabs?.map((item, index) => (
+                <Box
+                  key={index}
+                  sx={{
+                    cursor: "pointer",
+                    borderBottom: activeIndex === index && "1px solid #fff",
+                  }}
+                  onClick={() => handleClick(index)}
+                >
+                  <Typography variant="subtitle1"> {item}</Typography>
+                </Box>
+              ))}
+            </TabsWrapper>
+          </Box>
+        </Grid>
+        <Grid item xs={12} container spacing={1} mt={2}>
+          {compToShow()}
+        </Grid>
       </Grid>
       <RequestModal open={openRequestModal} handleClose={handleCloseModal} />
-    </Grid>
+    </>
   );
 };
 

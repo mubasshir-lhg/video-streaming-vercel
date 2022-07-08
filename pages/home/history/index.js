050f3697ee0fcbcaec767ae669d68f02c6fe4368
Murad <murad.jaddoon@gmail.com>
2022-07-08 19:39:48 +0500

    added confirmation modal to history page

--- pages/home/history/index.js
+++ pages/home/history/index.js
@@ -4,20 +4,31 @@ import { useRouter } from "next/router";
 import { RelatedVideosData } from "../../../_mockup/RalatedVideos";
 import SearchVideoResult from "../../../components/Video/searchVideoResult/searchVideoResult";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { ConfirmationModal } from "../../../components/Modal/ConfirmationModal/ConfirmationModal";
 
 const History = () => {
   const router = useRouter();
+  const [open, setOpen] = useState(false);
   const [data, setData] = useState(RelatedVideosData);
 
   const playVideo = () => {
     router.push("/home/playVideo");
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const Confirm = () => {
+    handleClose();
   };
   return (
     <Grid container spacing={2} mt={2}>
       <Grid xs={12} px={2}>
         <Typography variant="subtitle1">Watch history</Typography>
       </Grid>
-      <Grid item xs={12} sm={8} order={{xs:2,sm:1}}>
+      <Grid item xs={12} sm={8} order={{ xs: 2, sm: 1 }}>
         {data?.map((item, index) => (
           <Grid item xs={12} key={index} sx={{ mt: 1, pr: 2 }}>
             <SearchVideoResult
@@ -33,25 +44,37 @@ const History = () => {
         item
         xs={12}
         sm={4}
-        order={{xs:1,sm:2}}
+        order={{ xs: 1, sm: 2 }}
         sx={{
           backgroundColor: "primary.main",
           display: "flex",
           flexDirection: "column",
           gap: "10px",
-          p:2,
+          p: 2,
         }}
       >
-        <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
+        <Button
+          variant="outlined"
+          color="error"
+          startIcon={<DeleteIcon />}
+          onClick={handleOpen}
+        >
           Clear all watch history
         </Button>
-        <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
-          Clear last 1 hour watch history
-        </Button>
-        <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
-          Clear last 24 hours watch history
+        <Button
+          variant="outlined"
+          color="error"
+          startIcon={<DeleteIcon />}
+          onClick={handleOpen}
+        >
+          Clear all Comment history
         </Button>
       </Grid>
+      <ConfirmationModal
+        open={open}
+        handleClose={handleClose}
+        Confirm={Confirm}
+      />
     </Grid>
   );
 };

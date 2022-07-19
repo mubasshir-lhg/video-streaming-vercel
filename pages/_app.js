55636fe0861ec33fe8d4a81d742d40cd9c53d002
Murad <murad.jaddoon@gmail.com>
2022-07-19 19:45:19 +0500

    configg playContext

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,35 @@
-import 'regenerator-runtime/runtime';
+import "regenerator-runtime/runtime";
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import ThemeConfig from "../theme";
 import Layout from "../layout/Layout";
 import AuthLayout from "../layout/AuthLayout";
 import Axios from "axios";
-import { UserContext } from "../context/userContext";
+import { UserContext, PlayContext } from "../context/userContext";
 import { useState, useEffect } from "react";
 
 Axios.defaults.baseURL = "http://localhost:80/";
 
 function MyApp({ Component, pageProps, router }) {
   const [user, setUser] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
   useEffect(() => {}, []);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <ThemeConfig>
-        {router.pathname.startsWith("/auth") ? (
-          <AuthLayout>
-            <Component {...pageProps} />
-          </AuthLayout>
-        ) : (
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        )}
-      </ThemeConfig>
+      <PlayContext.Provider value={{ isPlaying, setIsPlaying }}>
+        <ThemeConfig>
+          {router.pathname.startsWith("/auth") ? (
+            <AuthLayout>
+              <Component {...pageProps} />
+            </AuthLayout>
+          ) : (
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          )}
+        </ThemeConfig>
+      </PlayContext.Provider>
     </UserContext.Provider>
   );
 }

98046dfb72cfa5983d5c73253632d471041ab585
Murad <murad.jaddoon@gmail.com>
2022-07-19 13:57:33 +0500

    config regenerator-runtime

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,32 @@
+import 'regenerator-runtime/runtime';
 import "../styles/globals.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import ThemeConfig from "../theme";
 import Layout from "../layout/Layout";
 import AuthLayout from "../layout/AuthLayout";
-import Axios from "axios"
+import Axios from "axios";
 import { UserContext } from "../context/userContext";
-import { useState,useEffect } from "react";
-Axios.defaults.baseURL="http://localhost:80/"
+import { useState, useEffect } from "react";
+
+Axios.defaults.baseURL = "http://localhost:80/";
 
 function MyApp({ Component, pageProps, router }) {
-  const[user,setUser] = useState(null);
-  useEffect(() => {
-  }, []);
+  const [user, setUser] = useState(null);
+  useEffect(() => {}, []);
 
   return (
-    <UserContext.Provider value={{user,setUser}} >
-    <ThemeConfig>
-      {
-       router.pathname.startsWith("/auth")? <AuthLayout>
-        <Component {...pageProps} />
-      </AuthLayout>:<Layout>
-        <Component {...pageProps} />
-      </Layout>
-      }
-      
-    </ThemeConfig>
+    <UserContext.Provider value={{ user, setUser }}>
+      <ThemeConfig>
+        {router.pathname.startsWith("/auth") ? (
+          <AuthLayout>
+            <Component {...pageProps} />
+          </AuthLayout>
+        ) : (
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        )}
+      </ThemeConfig>
     </UserContext.Provider>
   );
 }

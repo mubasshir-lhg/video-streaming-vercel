145096f59b94c1d2d08a3765627d7a79e771f92a
Murad <murad.jaddoon@gmail.com>
2022-07-21 15:41:54 +0500

    updated video comp

--- components/Video/videoToPlay/VideoWithVoiceControl.js
+++ components/Video/videoToPlay/VideoWithVoiceControl.js
@@ -2,26 +2,33 @@ import React, { useEffect, useRef } from "react";
 import { Box } from "@mui/material";
 import { useTheme } from "@mui/styles";
 
-const VideoWithVoiceControl = ({ src, controls, width, borderRadius, mx, my ,isPlaying}) => {
+const VideoWithVoiceControl = ({
+  src,
+  controls,
+  width,
+  borderRadius,
+  mx,
+  my,
+  isPlaying,
+}) => {
   const { shape, customShadows } = useTheme();
   const videoRef = useRef();
- 
-  if(isPlaying){
-    videoRef.current?.play()
-  }else{
-    videoRef.current?.pause()
-  }
 
   useEffect(() => {
     videoRef.current?.load();
-    console.dir(videoRef.current)
-  }, [src]);
+    if (isPlaying) {
+      videoRef.current?.play();
+    } else {
+      videoRef.current?.pause();
+    }
+  }, [src,isPlaying]);
   return (
     <Box sx={{ width: width || 240, my: my || 2, mx: mx || 0 }}>
       <video
         ref={videoRef}
         width="100%"
         loop
+        muted="muted"
         controls={controls || false}
         style={{
           borderRadius: borderRadius || shape.borderRadiusMd,

071945f70e46ab718581065491ea0517bf4467eb
Murad <murad.jaddoon@gmail.com>
2022-07-27 15:18:25 +0500

    added reactPlayer and remove video tag

--- components/Video/videoToPlay/VideoWithVoiceControl.js
+++ components/Video/videoToPlay/VideoWithVoiceControl.js
@@ -1,41 +1,20 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { Box } from "@mui/material";
-import { useTheme } from "@mui/styles";
-const VideoWithVoiceControl = ({
-  src,
-  controls,
-  width,
-  borderRadius,
-  mx,
-  my,
-  isPlaying,
-}) => {
-  const { shape, customShadows } = useTheme();
-  const videoRef = useRef();
-  useEffect(() => {
-    if (isPlaying) {
-      videoRef.current?.play();
-    } else {
-      videoRef.current.pause();
-    }
-  }, [isPlaying]);
-  useEffect(() => {
-    videoRef.current?.load();
-  }, [src]);
+import ReactPlayer from "react-player";
+const VideoWithVoiceControl = ({ src, width, mx, my, isPlaying }) => {
+  console.log("isPlaying", isPlaying);
   return (
     <Box sx={{ width: width || 240, my: my || 2, mx: mx || 0 }}>
-      <video
-        ref={videoRef}
+      <ReactPlayer
+        url={src}
+        muted
+        controls
+        playing={isPlaying}
         width="100%"
+        height="100%"
         loop
-        controls={controls || false}
-        style={{
-          borderRadius: borderRadius || shape.borderRadiusMd,
-          boxShadow: customShadows.z1,
-        }}
-      >
-        <source src={src} type="video/mp4" />
-      </video>
+        className="react-player"
+      />
     </Box>
   );
 };
